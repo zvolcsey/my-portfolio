f7dd3ca47ec4ecb2062d6324ddf8d758e173a7a5
Zoltán Völcsey <zvolcsey@gmail.com>
2022-05-14 05:58:50 +0200

    working on the footer component

--- src/components/Layouts/Footer.tsx
+++ src/components/Layouts/Footer.tsx
@@ -1,11 +1,38 @@
 import { FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './Footer.module.css';
+import Brand from '../Brand/Brand';
+import {
+  faCodepen,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Footer: FC<{}> = () => {
   return (
     <footer className={styles.footer}>
-      <p>Footer</p>
+      <Brand className={styles.brand} />
+      <ul className={styles.items}>
+        <li className={styles.item}>
+          <a
+            href='https://www.linkedin.com/in/zvolcsey'
+            className={styles.link}
+          >
+            <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a href='https://github.com/zvolcsey' className={styles.link}>
+            <FontAwesomeIcon icon={faGithub} /> GitHub
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a href='https://codepen.io/zoltanv' className={styles.link}>
+            <FontAwesomeIcon icon={faCodepen} /> CodePen
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 };
