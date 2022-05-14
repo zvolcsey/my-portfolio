4d4c2d4e10f49c3a9129d95a0af02acff4843869
Zoltán Völcsey <zvolcsey@gmail.com>
2022-05-14 05:09:06 +0200

    working on the home page

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,18 +1,20 @@
 import { FC, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import styles from './Layout.module.css';
 import Header from './Header';
+import Footer from './Footer';
 
 const Layout: FC<{}> = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className={styles.main}>
         <Suspense fallback={<p>Loading...</p>}>
           <Outlet />
         </Suspense>
       </main>
-      {/*Footer*/}
+      <Footer />
     </>
   );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,38 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { faArrowRightLong, faEye } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import styles from './HomePage.module.css';
 
 const HomePage: FC<{}> = () => {
-  return <p>Home Page</p>;
+  return (
+    <div className={styles['home-page']}>
+      <div className={styles.container}>
+        <span className={styles.badge}>Full Stack Developer</span>
+        <div className={styles['hero-text']}>
+          <p>
+            <span>From</span> Client
+          </p>
+          <p>
+            <span>To</span> 'under the hood'
+          </p>
+        </div>
+        <ul className={styles.actions}>
+          <li className={styles.item}>
+            <Link to='/projects' className={`${styles.link} ${styles.primary}`}>
+              <FontAwesomeIcon icon={faEye} /> See my projects
+            </Link>
+          </li>
+          <li className={styles.item}>
+            <Link to='/about' className={`${styles.link} ${styles.secondary}`}>
+              Who I am? <FontAwesomeIcon icon={faArrowRightLong} />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
