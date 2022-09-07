a85f0949ab3e481e1682d65d87d0eec52780c3a6
Zoltán Völcsey <zvolcsey@gmail.com>
2022-09-07 19:11:30 +0200

    Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest'],
+  root: true,
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'typescript-eslint/ban-types': 'off',
+  },
+  env: {
+    'jest/globals': true,
+    browser: true,
+  },
+  settings: {
+    jest: {
+      version: 27,
+    },
+  },
+};
